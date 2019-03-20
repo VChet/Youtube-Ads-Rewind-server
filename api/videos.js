a407004b7a4ba9e86c4b2d28e98a881ba7efeca8
VChet <17050347+VChet@users.noreply.github.com>
2019-03-20 23:39:24 +0500

    Push timings to the storage

--- api/videos.js
+++ api/videos.js
@@ -4,9 +4,11 @@ module.exports = {
 };
 
 const axios = require("axios");
-const { youtube } = require("../config");
+const config = require("../config");
+
 const { Video } = require("../models/video");
 const { Channel } = require("../models/channel");
+const { Storage } = require("../models/storage");
 
 function getList(req, res) {
   Video.find({}, (err, videos) => {
@@ -15,29 +17,55 @@ function getList(req, res) {
   });
 }
 
-function addTiming(req, res) {
+async function addTiming(req, res) {
   const { id, timings } = req.body;
   console.log({ ip: req.ip, id, timings});
-  axios.get(`${youtube.api}/videos`, {
+  // 1. Check if we are ok to store videos from this channel
+  let channelId = await axios.get(`${config.youtube.api}/videos`, {
     params: {
       part: "snippet",
       id,
-      key: youtube.key
+      key: config.youtube.key
     }
   }).then(response => {
-    const channelId = response.data.items[0].snippet.channelId;
-    console.log({channelId});
-    Channel.findOne({channelId}, (channelError, channel) => {
-      if (channelError) return res.status(500).json({ channelError });
-      if (!channel) return res.status(400).json({ response: "This channel is unavailable" });
-
-      const newTiming = new Video({ id, timings });
-      newTiming.save(timingError => {
-        if (timingError) return res.status(500).json({ timingError });
-        return res.status(201).json({ response: newTiming });
-      });
-    });
+    return response.data.items[0].snippet.channelId;
   }).catch(error => {
     console.log(error.response.data);
   });
+
+  const channel = await Channel.findOne({channelId}).lean();
+  if (!channel) return res.status(400).json({ response: "This channel is unavailable" });
+  // 2. Check if this video is already stored in the storage
+  const storageVideo = await Storage.findOne({ id }).lean();
+  // 2.1. If not - add new record and return
+  if (!storageVideo) {
+    const newTiming = new Storage({ id, timings });
+    newTiming.save(timingError => {
+      if (timingError) console.log("Error adding new timing", timingError);
+      console.log("New timing has been added", { id, timings });
+      res.status(201).json({ response: "New timing has been added to the storage" });
+    });
+    return;
+  }
+  // 2.2. Otherwise, push to the existing storage
+  const pushTiming = await Storage.findOneAndUpdate({ id }, { $push: { timings }});
+  console.log("Timing has been pushed to the existing storage", timings);
+  // 3. Count timings for this video
+  const pipeline = [
+    { $match: { id } },
+    { $project: {
+      size: { $size: "$timings" }
+    }}
+  ];
+  const storageTimings = await Storage.aggregate(pipeline);
+  const timingsCounter = storageTimings[0].size;
+  console.log({ timingsCounter });
+  // 4. If storage limit is not exceeded - return message
+  if (timingsCounter < config.storageLimit) return res.status(201).json({
+    response: "Timing has been pushed to the existing storage",
+    timings
+  });
+  // 5. Otherwise, check existing storage to find duplicates
+  // TODO: Find duplicates and update main collection
+  console.log({ timings: storageVideo.timings });
 }
--- config.js
+++ config.js
@@ -9,5 +9,7 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
   appPort: "7542",
   mongoUrl: "mongodb://localhost/youtube_ads",
+  storageLimit: 10,
+  timingDifference: 3,  // in seconds
   ...overwriteConfig
 };
